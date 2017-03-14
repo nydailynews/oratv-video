98b7e50d82d0197bf6a2591c1ae5a9278a8e86db
freejoe76 <jmurphy@denverpost.com>
2017-03-14 13:34:13 -0400

    Get rudimentary channel view working

--- www/channel/mike-rogers-world-war-e.php
+++ www/channel/mike-rogers-world-war-e.php
@@ -1,1 +1,16 @@
 <?php
+$channel_local = array(
+	'TITLE' => '“World War E” with Mike Rogers', 
+	'DESCRIPTION' => 'National Security expert Mike Rogers takes you inside “World War E” and the borderless battleground where cyber-thieves, terrorists and nations use the internet to wage a war for money, power and information.', 
+	'DESCRIPTION_LONG' => '<p>In just two decades, the Internet went from novelty to necessity, connecting the world like no other medium.  While it is a powerful tool for information and communication, it also has a dark, shadowy side with the potential to disrupt or destroy.</p>
+<p>National Security expert Mike Rogers explores the dangers of life in the digital age in “World War E.”</p>
+<p>Rogers draws upon his experience at the highest levels of the intelligence community to explain why cyberspace is the new battlefield between nations, citing specific examples where China and Russia and other countries have cyber-meddled in U.S. Affairs.</p>
+<p>He details the billion-dollar-a-year business of digital extortion, one that threatens to destroy or disrupt critical infrastructure like medical and police records and even your own personal information.</p>
+<p>He’ll discuss how your digital footprints leave a trail of personal data available for cyber-thieves and terrorists to trade and sell in the Dark Web.</p>
+<p>The electronic world has a profound effect on your personal security and privacy...but you have to know how it works to protect yourself.</p>
+<p>Mike Rogers analyzes the threats and gives the best case, worst case, and most likely scenarios in “World War E.”</p>', 
+	'SHORTURL' => '',
+	'KEYWORDS' => '', 
+	'CANONICALURL' => '', 
+	'IMGNAME' => ''
+);
--- www/views.php
+++ www/views.php
@@ -58,7 +58,7 @@ class Request {
 			'CONTENT' => file_get_contents('content/index.html'),
 		);
 		$this->template_vars = array_merge($this->template_vars, $local);
-		$this->markup = $this->build_response();
+		$this->markup = $this->populate_markup();
 		echo $this->markup;
 	}
 
@@ -71,18 +71,14 @@ class Request {
 			'CONTENT' => file_get_contents('content/channel.html'),
 		);
 		// This include will populate the $channel_local array.
+		// We also need some of the $channel_local vars in the parent-template array,
+		// so we merge that later on too.
 		include('channel/' . $channel . '.php');
-		$local['CONTENT'] = $this->build_channel($channel, $channel_local, $local['CONTENT']);
 
-		$this->template_vars = array_merge($this->template_vars, $local);
-		$this->markup = $this->build_response();
+		$this->template_vars = array_merge($this->template_vars, $local, $channel_local);
+		$this->template_vars['CONTENT'] = $this->populate_markup($local['CONTENT']);
+		$this->markup = $this->populate_markup();
 		echo $this->markup;
-	}
-
-	function build_channel($channel, $local, $content)
-	{
-		// Replace the vars in the page content with actual content.
-		return str_replace(array_keys($this->template_vars), array_values($this->template_vars), $content);
 	}
 
 	function return_404()
@@ -94,14 +90,18 @@ class Request {
 			'CONTENT' => file_get_contents('content/404.html'),
 		);
 		$this->template_vars = array_merge($this->template_vars, $local);
-		$this->markup = $this->build_response();
+		$this->markup = $this->populate_markup();
 		header('HTTP/1.0 404 Not Found');
 		echo $this->markup;
 	}
 
-	function build_response()
+	function populate_markup($markup='')
 	{
-		$markup = str_replace(array_keys($this->template_vars), array_values($this->template_vars), $this->markup);
-		return $markup;
+		// Replace a string of markup with the template vars array.
+		// If $markup is not passed the function will default to the $this->markup value.
+		if ( $markup == '' ) $markup = $this->markup;
+
+		$return = str_replace(array_keys($this->template_vars), array_values($this->template_vars), $markup);
+		return $return;
 	}
 }
