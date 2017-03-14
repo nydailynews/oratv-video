9a71e6f7a99bec8c980788ba0f00eb8444afe846
freejoe76 <jmurphy@denverpost.com>
2017-03-14 12:17:10 -0400

    Get 404 view working

--- www/views.php
+++ www/views.php
@@ -9,7 +9,7 @@
 
 // Sanitize and check input
 $vendor = htmlspecialchars($_GET['vendor']);
-$channel = htmlspecialchars($_GET['channel']
+$channel = htmlspecialchars($_GET['channel']);
 $slug = htmlspecialchars($_GET['slug']);
 
 $approved_vendors = ['ora'];
@@ -24,16 +24,40 @@ class Request {
 
 	// A Request object takes all the information needed to put together a response
 	// and returns the response.
+	var $template_vars;
+	var $markup;
 
 	function __construct()
 	{
+		$this->template_vars = array(
+			'TITLE' => '', 
+			'DESCRIPTION' => '', 
+			'SHORTURL' => '',
+			'KEYWORDS' => '', 
+			'CANONICALURL' => '', 
+			'IMGNAME' => '');
+		$this->markup = file_get_contents('blank.html');
 	}
 
 	function return_404()
 	{
+		$local_template_vars = array(
+			'TITLE' => 'Page not found', 
+			'DESCRIPTION' => 'This is your 404 page.', 
+			'SHORTURL' => '',
+			'KEYWORDS' => '', 
+			'CANONICALURL' => '', 
+			'IMGNAME' => '');
+		$merged = array_merge($this->template_vars, $local_template_vars);
+		$this->template_vars = $merged;
+		$this->markup = $this->build_response();
+		header('HTTP/1.0 404 Not Found');
+		echo $this->markup;
 	}
 
 	function build_response()
 	{
+		$markup = str_replace(array_keys($this->template_vars), array_values($this->template_vars), $this->markup);
+		return $markup;
 	}
 }
