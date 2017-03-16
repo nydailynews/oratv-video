9b21bb7a6c829b1b7795ae22abe9a30e1f659604
freejoe76 <jmurphy@denverpost.com>
2017-03-16 15:12:40 -0400

    Start on the script that compares CSVs and adds items from the new one to the old one if the new one isn't already in the old one

---
+++ addtocsv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Compare CSVs, adding any new items in the first CSV to the second CSV.
+import os
+import doctest
+import argparse
+import csv
+
+def main(args):
+    """ For command-line use.
+        """
+    if args:
+        articles = []
+        for arg in args.fns[0]:
+            if args.verbose:
+                print arg
+
+
+def build_parser():
+    """ We put the argparse in a method so we can test it
+        outside of the command-line.
+        """
+    parser = argparse.ArgumentParser(usage='$ python addtocsv.py http://domain.com/rss/',
+                                     description='''Takes a list of CSVs passed as args.
+                                                  Returns the items that are in the first one but not in the subsequent ones.''',
+                                     epilog='')
+    parser.add_argument("-v", "--verbose", dest="verbose", default=False, action="store_true")
+    parser.add_argument("fns", action="append", nargs="*")
+    return parser
+
+if __name__ == '__main__':
+    """ 
+        """
+    parser = build_parser()
+    args = parser.parse_args()
+
+    if args.verbose:
+        doctest.testmod(verbose=args.verbose)
+
+    main(args)
